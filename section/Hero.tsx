23d1fec2fc4664dc2a4e578f79b06bac5ef32f51
Aditya Kashyap <adityaraikwar@12789gmail.com>
2025-01-31 03:02:35 +0530

    partially completed hero section

--- section/Hero.tsx
+++ section/Hero.tsx
@@ -2,8 +2,16 @@ import React, { FC } from 'react'
 
 const Hero:FC = () => {
   return (
-    <section className='py-[72px]'>
-        <div className='~text-6xl/8xl' >Hero</div>
+    <section className='container mx-auto  h-screen  py-16'>
+        <div className='tablet:flex tablet:flex-col tablet:justify-center tablet:items-center tablet:text-center min-h-[calc(100%-50px)]'>
+          <div className='flex text-6xl laptop:text-7xl desktop:text-8xl  pb-7 tablet:max-w-[1200px] '>
+            <div className='leading-tight font-[350]'>Make <div className='inline-block bg-[rgb(243,100,88)] text-white px-3 laptop:px-5 '>content</div> your <span className='font-bold'>competitive advantage</span></div>
+          </div>
+          <div className='max-w-[800px]'>
+            <p className='text-2xl tablet:text-3xl font-[425]  leading-snug '>Expect more from your CMS:Treat content as data --actionable, scalable, and ready to drive your business  forward  with Sanity  Content Operating System.</p>
+          </div>
+
+        </div>
     </section>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,24 @@ const config: Config = {
     extract
   } ,
   theme: {
-    screens, // Tailwind's default screens, in `rem`
+    screens: {
+      'mobile':'640px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1440px',
+      'desktopl':'1920px',
+      '4k':'2560px'
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        tablet: '2rem',
+        laptop: '4rem',
+        desktop: '6rem',
+        desktopl:'8rem',
+        '4k': '20rem',
+      },
+    }, 
     fontSize,
     extend: {
       colors: {
