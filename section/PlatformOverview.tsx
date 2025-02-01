b345d994c9ab1d387d6bfe40072bd72d22f8216a
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-02-02 03:15:10 +0530

    40perc platform overview card completed

--- section/PlatformOverview.tsx
+++ section/PlatformOverview.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { FC } from 'react'
 import platformFirst from '@/assets/platformFirst.png'
 import platformSec from '@/assets/platformSec.png'
@@ -11,24 +13,31 @@ const PlatformOverview: FC = () => {
       <Image className=' absolute hidden xl:block  right-0' src={platformFirst} height={600} alt="1image" />
       <Image className=' absolute hidden xl:block left-0' src={platformSec} height={600} alt='2image' />
       <div className='my-16 [perspective:800px] [transform-style:preserve-3d]'>
-        {/* <div className=' mx-auto h-48 w-48 bg-white rotate-45 ' > */}
-        <div className="flex flex-col gap-4  bg-[rgb(39,42,46)] hover:bg-white transition mx-auto h-48 w-48 my-5 overflow-hidden " style={{ transform: "rotateX(60deg) rotate(-45deg)" }}>
+        <div className="flex flex-col gap-4  bg-[rgb(39,42,46)]  transition mx-auto h-48 w-48 my-5 overflow-hidden " style={{ transform: "rotateX(60deg) rotate(-45deg)" }}  onMouseEnter={() => console.log("enter")} onMouseLeave={()=>console.log("leave")}>
           <div className=' flex w-full h-1'>
             <div className='bg-[rgb(243,100,88)] w-3 h-3'></div>
             <div className='bg-[rgb(26,28,31)] w-full h-3'></div>
           </div>
-          <div className='flex gap-2 h-full p-2 '>
-            <div className=' flex flex-col gap-1 w-1/5 py-2 bg-[rgb(63,67,74)] hover:bg-slate-200 '>
-              <div className='w-full h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-              <div className='w-full h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-              <div className='w-full h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-              <div className='w-full h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-              <div className='w-full h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-              <div className='w-full h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-              <div className='w-[70%] h-[6px] bg-[rgb(86,93,103)] hover:bg-black'></div>
-            
+          <div className='flex gap-2 h-full p-[10px] '>
+            <div className=' flex flex-col gap-1 w-1/5  bg-[rgb(63,67,74)]'>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-[70%] h-[6px] bg-[rgb(86,93,103)] '></div>
             </div>
-            <div className='w-4/5 bg-[rgb(63,67,74)]'></div>
+            <div className='flex flex-col gap-1 w-4/5 bg-[rgb(63,67,74)] '>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-[70%] h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[40px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-[86%] h-[6px] bg-[rgb(86,93,103)] '></div>
+              <div className='w-full h-[6px] bg-[rgb(86,93,103)] '></div>
+            </div>
           </div>
         </div>
       </div>
