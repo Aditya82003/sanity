9a9f262a154475de6ba5e4828c920a3fad5b6ab3
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-02-04 18:01:07 +0530

    completed 50perc platform overview section

--- section/PlatformOverview.tsx
+++ section/PlatformOverview.tsx
@@ -11,13 +11,14 @@ const PlatformOverview: FC = () => {
   const sampleArray: number[] = Array.from({ length: 99 }, (_, i) => i + 1);
 
   return (
-    <section className='relative bg-black h-screen py-20 text-center'>
+    <section className='relative bg-black  py-20 text-center'>
       <h1 className='text-center font-bold text-3xl text-[rgb(235,120,109)]'>Platform Overview</h1>
       <h1 className='text-center my-1 text-white text-4xl lg:text-5xl xl:text-6xl  tracking-normal'>Sanity Content Operating System</h1>
       <Image className=' absolute hidden xl:block  right-0' src={platformFirst} height={600} alt="1image" />
       <Image className=' absolute hidden xl:block left-0' src={platformSec} height={600} alt='2image' />
       <div className='flex flex-col justify-center relative my-16 max-h-[500px] [perspective:800px] [transform-style:preserve-3d]'>
-        <div className=' relative inline-flex   '>
+
+        <div className=' relative inline-flex'>
           <div className="flex-col inline-flex gap-4  bg-[rgb(39,42,46)]  transition mx-auto h-48 w-48  overflow-hidden " style={{ transform: "rotateX(56deg) rotate(-45deg)" }} onMouseEnter={() => setMouseOver(true)} onMouseLeave={() => setMouseOver(false)}>
             <div className=' flex w-full h-1'>
               <div className='bg-[rgb(243,100,88)] w-3 h-3'></div>
@@ -45,14 +46,14 @@ const PlatformOverview: FC = () => {
               </div>
             </div>
           </div>
-          <div className='absolute pl-8 left-[70%] xl:left-[60%] max-w-[300px]'>
-            <h5 className='hidden md:block max-w-[100px] text-white bg-[rgb(19,20,27)] hover:bg-gray-800 px-4 py-2 rounded-full tracking-tighter font-semibold mb-4 '>Studio</h5>
-            <p className='hidden md:block  text-white text-start '>  A fully customizable content workspace that mirrors your business and unleashes velocity and creativity.</p>
+          <div className='absolute hidden md:block pl-8 left-[70%] xl:left-[60%] max-w-[300px]'>
+            <h5 className=' max-w-[100px] text-white bg-[rgb(19,20,27)] hover:bg-gray-800 px-4 py-2 rounded-full tracking-tighter font-semibold mb-4 '>Studio</h5>
+            <p className=' text-white text-start '>  A fully customizable content workspace that mirrors your business and unleashes velocity and creativity.</p>
           </div>
-
         </div>
+
         <div>
-          <div className='max-sm:hidden absolute top-[70%] left-[5%] xl:left-[30%] flex flex-col gap-2 items-end max-w-[200px] '>
+          <div className='max-sm:hidden absolute top-[70%] left-[5%] xl:left-[25%] flex flex-col gap-2 items-end max-w-[200px] '>
             <h5 className='  max-w-[100px] text-white bg-[rgb(19,20,27)] hover:bg-gray-800 px-4 py-2 rounded-full tracking-tighter font-semibold '>API</h5>
             <p className='  text-white text-end'>
               Powerful and intuitive interfaces that are the fabric of your content operating system</p>
@@ -64,6 +65,30 @@ const PlatformOverview: FC = () => {
 
           </div>
         </div>
+        <div className=''>
+        <div className='absolute  top-[80%] left-[26%] sm:left-[38%] lg:left-[40%] xl:left-[45%] -z-30' >
+          <div className=' absolute w-48 h-48 left-[45%] bg-black rounded-3xl  z-[9]' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[10px] bg-[rgb(110,118,131)] rounded-3xl z-[8]' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[20px] bg-black rounded-3xl z-[7] ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[30px] bg-[rgb(86,93,103)] rounded-3xl z-[6] ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[40px] bg-black rounded-3xl z-[5] ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[50px] bg-[rgb(63,67,74)] rounded-3xl z-[4] ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[60px] bg-black rounded-3xl z-[3] ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[70px] bg-[rgb(39,42,46)] rounded-3xl z-[2]' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[80px] bg-black rounded-3xl z-[1] ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+          <div className=' absolute w-48 h-48 left-[45%] top-[90px] bg-[rgb(110,118,131)] rounded-3xl z-0 ' style={{ transform: "rotateX(56deg) rotate(-45deg)" }}></div>
+        </div>
+        <div className='absolute pl-8 left-[70%] xl:left-[60%] max-w-[300px]'>
+            <h5 className='hidden md:block max-w-[100px] text-white bg-[rgb(19,20,27)] hover:bg-gray-800 px-4 py-2 rounded-full tracking-tighter font-semibold mb-4 '>Content</h5>
+            <p className='hidden md:block  text-white text-start '>  A fully customizable content workspace that mirrors your business and unleashes velocity and creativity.</p>
+          </div>
+              
+        </div>
+
+      </div>
+      <h1 className='text-5xl sm:text-7xl font-normal text-white mt-64 tracking-tighter'>Welcome to the Composable Era</h1>
+      <div>
+        
       </div>
     </section >
   )
